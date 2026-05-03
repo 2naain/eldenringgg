bf0f201cc200d6585d52ed1fe94f5056d753db3e
2naain <jdlozano31@ucatolica.edu.co>
2026-05-03 17:21:22 -0500

    endpoints of characters donde, now comming up with weapons

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException
 
+from models import CharacterUpdate
+
 app = FastAPI(
     title="Elden Ring  API",
     description="Elden Ring build manager API",
@@ -25,3 +27,32 @@ async def create_character(character:CharacterBase):
 @app.get("/character/filter/class", response_model=list[CharacterID], tags=["Character"])
 async def filter_characters(characters_class: str):
     return filterCharacters(characters_class)
+
+@app.get("/character/search/name", response_model=CharacterID, tags=["Character"])
+async def search_character(name: str):
+    character= searchCharacterByName(name)
+    if not character:
+        raise HTTPException(status_code=404, detail=f"Character '{name}' not found")
+    return character
+
+@app.get("/character/{id}", response_model=CharacterID, tags=["Character"])
+async def show_character(id: str):
+    character= findCharacter(id)
+    if not character:
+        raise HTTPException(status_code=404, detail=f"Character '{id}' Character not found")
+    return character
+
+@app.patch("/character/{id}", response_model=CharacterID, tags=["Character"])
+async def update_character(id: int , character: CharacterUpdate):
+    updated = updateCharacter(id, character)
+    if not updated:
+        raise HTTPException(status_code=404, detail=f"Character '{id}' Character not found")
+    return updated
+
+@app.delete("/character/{id}", response_model=CharacterID, tags=["Characters"])
+async def delete_character(id: int):
+    deleted = deleteCharacter(id)
+    if not deleted:
+        raise HTTPException(status_code=404, detail=f"{id} Character not found")
+    return deleted
+
